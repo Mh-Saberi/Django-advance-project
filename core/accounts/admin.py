bfd3dc62324470fb79f307c8c8d39bd55a77eeb3
Mahtab Saberi <saberimahtab2002@gmail.com>
2026-07-02 21:57:11 -0700

    add amin forms

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -8,6 +8,26 @@ class CustomUserAdmin(UserAdmin):
     list_filter = ('email', 'is_staff', 'is_active')
     search_fields = ('email',)
     ordering = ('email',)
+    fieldsets = (
+        ('Authentication', {
+            "fields": (
+                'email', 'password'
+            ),
+        }),
+        ('permissions', {
+            "fields": (
+                'is_staff', 'is_active', 'is_superuser'
+            ),
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff", "is_active", "is_superuser"
+            )}
+        ),
+    )
 
 
 admin.site.register(User, CustomUserAdmin)
