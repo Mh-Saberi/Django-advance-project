38b275a3549eb56090a4af8219e6e9d5476da31e
Mahtab Saberi <saberimahtab2002@gmail.com>
2026-07-02 21:06:26 -0700

    add adminpanel for custom user

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 class CustomUserAdmin(UserAdmin):
-    
+    model = User
+    list_display = ('email', 'is_staff', 'is_active')
+    list_filter = ('email', 'is_staff', 'is_active')
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.register(User, CustomUserAdmin)
